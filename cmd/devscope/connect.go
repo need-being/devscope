bf4f9d078295f681056291c26858473c139b2742
Need-Being <32161782+need-being@users.noreply.github.com>
2020-12-27 20:50:35 +0800

    implement client

--- cmd/devscope/connect.go
+++ cmd/devscope/connect.go
@@ -1,14 +1,45 @@
 package main
 
 import (
+	"errors"
+	"io"
+	"net"
+	"os"
+	"os/exec"
+
+	"github.com/apex/log"
 	"github.com/urfave/cli/v2"
 )
 
 var connectCommand = &cli.Command{
-	Name:   "connect",
-	Action: runConnect,
+	Name:      "connect",
+	UsageText: "devscope connect [command options] <address>",
+	Action:    runConnect,
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:    "shell",
+			Aliases: []string{"s"},
+			Value:   "/bin/sh",
+		},
+	},
 }
 
 func runConnect(ctx *cli.Context) error {
-	return nil
+	address := ctx.Args().First()
+	if address == "" {
+		return errors.New("missing argument: address")
+	}
+
+	conn, err := net.Dial("tcp", address)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+	log.Infof("Connected to %v", address)
+
+	cmd := exec.Command(ctx.String("shell"))
+	cmd.Stdin = io.TeeReader(conn, os.Stdout)
+	cmd.Stdout = io.MultiWriter(conn, os.Stdout)
+	cmd.Stderr = io.MultiWriter(conn, os.Stderr)
+	return cmd.Run()
 }
